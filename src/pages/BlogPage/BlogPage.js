cfe15875be4a9a4684c89fa3f9fa8683cc0a91b8
alex4444-dev <alexgrig406@gmail.com>
2022-10-09 13:07:27 +0000

    fixed bug when action require refresh page

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -1,70 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import s from './BlogPage.module.css';
+import React, { useEffect, useState } from "react";
+import s from "./BlogPage.module.css";
 
-import { Post } from './Post/Post';
-import { PostsHeader } from './PostsHeader/PostsHeader';
-import { fetchPosts } from '../../store/slices/posts';
-import { Link } from 'react-router-dom';
-import { POSTS_URL } from '../../utils/constants';
-import { useDispatch } from 'react-redux';
-import { EditForm } from '../../components/EditForm/EditForm';
+import { Post } from "./Post/Post";
+import { PostsHeader } from "./PostsHeader/PostsHeader";
+import {
+  deletePost,
+  editPost,
+  fetchPosts,
+  selectPostsData,
+} from "../../store/slices/posts";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { EditForm } from "../../components/EditForm/EditForm";
 
 export const BlogPage = ({
   title,
   blogPosts,
-  isLoading,
-  setBlogPosts,
-  error,
-  isLikedPosts = false,
-  isAdmin
+  setBlogPosts, 
+  isLikedPosts = false, 
+  isAdmin 
 }) => {
-  const likedPosts = blogPosts.filter((post) => post.liked);
-
-  const likePost = (pos) => {
-    const updatedPosts = [...blogPosts];
-    updatedPosts[pos].liked = !updatedPosts[pos].liked;
-    fetch(POSTS_URL + updatedPosts[pos].id, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedPosts[pos]),
-    })
-      .then((res) => res.json())
-      .then((updatedPostFromServer) => {
-        updatedPosts[pos] = updatedPostFromServer;
-        setBlogPosts(updatedPosts);
-      })
-      .catch((error) => console.log(error));
-  };
-
-
+  
+  
+  const {list: posts, isLoading, error} = useSelector(selectPostsData);
   const dispatch = useDispatch();
+
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
 
+  
 
-  const deletePost = (postId) => {
-    const isDelete = window.confirm('Удалить пост?');
+  const likedPosts = posts.filter((post) => post.liked);
+
+  const handleLikePost = (index) => {
+    const updatedPosts = [...posts];
+    updatedPosts[index] = {
+      ...updatedPosts[index],
+      liked: !updatedPosts[index].liked
+    };
+    dispatch(editPost(updatedPosts[index]))
+  };
+
+  const handleDeletePost = (postId) => {
+    const isDelete = window.confirm("Удалить пост?");
 
     if (isDelete) {
-      fetch(POSTS_URL + postId, { method: 'DELETE' })
-        .then(() =>
-          setBlogPosts(blogPosts.filter((post) => post.id !== postId))
-        )
-        .catch((error) => console.log(error));
+      dispatch(deletePost(postId)); 
     }
+    
   };
 
   const [selectedPost, setSelectedPost] = useState({});
   const [showEditForm, setShowEditForm] = useState(false);
 
-  const selectPost = (post) => {
-    setSelectedPost(post);
-    setShowEditForm(true);
-  };
+    const selectPost = (post) => {
+      setSelectedPost(post);
+      setShowEditForm(true);
+    };
 
   if (isLoading) return <h1>Получаем данные...</h1>;
 
@@ -76,27 +70,26 @@ export const BlogPage = ({
         title={title}
         isLikedPosts={isLikedPosts}
         setBlogPosts={setBlogPosts}
-        blogPosts={blogPosts}
+        blogPosts={posts}
         isAdmin={isAdmin}
       />
 
       <section className={s.posts}>
-        {(isLikedPosts ? likedPosts : blogPosts).map((post, pos) => {
+        {(isLikedPosts ? likedPosts : posts).map((post, pos) => {
           return (
             <React.Fragment key={post.id}>
               <Post
-                title={post.title}
-                description={post.description}
-                liked={post.liked}
-                likePost={() => likePost(pos)}
-                deletePost={() => deletePost(post.id)}
+                {...post}
+                likePost={() => handleLikePost(pos)}
+                deletePost={() => handleDeletePost(post.id)}
                 selectPost={() => selectPost(post)}
                 isAdmin={isAdmin}
               />
-              <div className={s.readMoreLink}><Link to={`/blog/${post.id}`}>Подробнее</Link></div>
+              <div className={s.readMoreLink}>
+                <Link to={`/blog/${post.id}`}>Подробнее</Link>
+              </div>
             </React.Fragment>
           );
-
         })}
       </section>
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,3 @@
-
-
 export const POSTS_URL = 'https://618d258eedab980017fd521e.mockapi.io/api/posts/';
 
 export const APP_ROUTES = [
